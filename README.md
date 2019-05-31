6245cdf7c9e5220a448919fa6436ecddbda95b3d
Steven Masfaraud <masfaraud@dessia.tech>
2019-05-31 14:16:44 +0200

    Script enhancement

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # drone-seafile
+
+https://img.shields.io/github/stars/Dessia-tech/drone-seafile.svg?style=social
+
 A Drone plugin for uploading files to a seafile server
+
+
+## Usage
+
+
+pipeline:
+  seafile-upload:
+    image: dessia/drone-seafile
+    settings:
+      server_url: https://seafileserver.tld
+      repo_id: 
+        from_secret: repo_id
+      seafile_token:
+        from_secret: seafile_token
+      file: upload_test.md
+      root_directory: '/subdir/'
--- seafile-upload.sh
+++ seafile-upload.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-curl -H "Authorization: Token ${PLUGIN_SEAFILE_TOKEN}" $PLUGIN_SERVER_URL
-upload_link=`curl -H "Authorization: Token ${PLUGIN_SEAFILE_TOKEN}" $PLUGIN_SERVER_URL | sed -e 's/^"//' -e 's/"$//'`;
-curl -v -H "Authorization: Token ${PLUGIN_SEAFILE_TOKEN}" -F file="@${PLUGIN_FILE}" -F parent_dir=$PLUGIN_ROOT_DIRECTORY;
+upload_link=`curl -H "Authorization: Token ${PLUGIN_SEAFILE_TOKEN}" "${PLUGIN_SERVER_URL}/api2/repos/${PLUGIN_REPO_ID}/upload-link/" | sed -e 's/^"//' -e 's/"$//'`
+for filename in $PLUGIN_FILES; do
+    echo "uploading ${filename}";
+    curl -H "Authorization: Token ${PLUGIN_SEAFILE_TOKEN}" -F file="@${filename}" -F parent_dir=$PLUGIN_ROOT_DIRECTORY  -F replace=1  $upload_link
+done
